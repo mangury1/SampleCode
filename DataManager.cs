91f95c4c66dcd9f9092a13b153c025f0988c36d8
mangury1 <jumi5808@nate.com>
2026-02-07 02:15:47 +0900

    Add files via upload

---
+++ DataManager.cs
@@ -0,0 +1,19 @@
+﻿using Barebones.MasterServer;
+using System;
+using UnityEngine;
+
+public static class DataManager
+{
+    public static UserDataManager UserDataManager { get; private set; }
+
+    public static void Login(AccountInfoPacket accountInfoPacket)
+    {
+        UserDataManager = new UserDataManager(accountInfoPacket);
+    }
+
+    public static void Logout()
+    {
+        UserDataManager.Release();
+        UserDataManager = null;
+    }
+}
